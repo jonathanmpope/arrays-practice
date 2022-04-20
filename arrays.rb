15a498d2a479cc1d73847a2ceba217596d9e0592
Jonathan Pope <jonathanmpope@gmail.com>
2022-04-20 09:35:48 -0600

    Add methods for each array

--- arrays.rb
+++ arrays.rb
@@ -1,11 +1,15 @@
-# String
 fenrir_nicknames = ["toast", "big handsome", "gq mal", "land shark"]
+# the .pop method with an argument of 2 will remove the last two elements and return them
+fenrir_nicknames.pop(2)
 
-# Integers
 tacos_per_day = [3, 6, 1, 0]
+# the .push method will add the 2 arguments provided to the end of the existing array
+tacos_per_day.push(4, 3)
 
-# Floats
 money = [100.20, 43.59, 0.78, 3338.87]
+# the .shift method will remove and return the first two elements (an argument of 2 is provided)
+money.shift(2)
 
-# Booleans
 scared_of_fenrir = [true, true, false, true]
+# the unshift method will add the element (false) to the beginning of the array
+scared_of_fenrir.unshift(true)
