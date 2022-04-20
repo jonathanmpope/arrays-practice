d0d518ce6e01e59db21ef60f4bd79e19d37f90bd
Jonathan Pope <jonathanmpope@gmail.com>
2022-04-20 09:28:16 -0600

    Add arrays

---
+++ arrays.rb
@@ -0,0 +1,11 @@
+# String
+fenrir_nicknames = ["toast", "big handsome", "gq mal", "land shark"]
+
+# Integers
+tacos_per_day = [3, 6, 1, 0]
+
+# Floats
+money = [100.20, 43.59, 0.78, 3338.87]
+
+# Booleans
+scared_of_fenrir = [true, true, false, true]
