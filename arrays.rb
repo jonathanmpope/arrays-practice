9312992eacde1b2eccc976115b4992104cf3ab47
Jonathan Pope <jonathanmpope@gmail.com>
2022-04-20 09:51:16 -0600

    Add another array method

--- arrays.rb
+++ arrays.rb
@@ -13,3 +13,15 @@ money.shift(2)
 scared_of_fenrir = [true, true, false, true]
 # the unshift method will add the element (false) to the beginning of the array
 scared_of_fenrir.unshift(false)
+
+#below I'll demo my understanding of index positions
+#the statement below will return 43.59
+money[1]
+#the statement below will return "toast"
+fenrir_nicknames[0]
+#the statement below will return 0
+tacos_per_day[3]
+
+#the insert method will add 4 and 5 at the 2 index position. The new array will be 2, 3, 4, 5, 6, 7
+numbers = [2, 3, 6, 7]
+numbers.insert(2, 4, 5)
